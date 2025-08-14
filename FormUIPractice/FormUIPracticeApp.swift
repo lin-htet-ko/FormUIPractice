da70c21e2eda165c887d7c10d37649e2df126da1
Lin Htet Ko <152836962+linhtetko-codigo@users.noreply.github.com>
2025-08-14 20:19:03 +0700

    Initial Commit

---
+++ FormUIPractice/FormUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormUIPracticeApp.swift
+//  FormUIPractice
+//
+//  Created by Lin Htet Ko Macbook Pro on 14/8/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct FormUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
